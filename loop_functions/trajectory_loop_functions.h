5fbcb170109f867f9aead051aca6f7028715d9e9
BCFrancis <bfrancis@wpi.edu>
2023-04-23 12:17:20 -0400

    debugging

---
+++ loop_functions/trajectory_loop_functions.h
@@ -0,0 +1,40 @@
+#ifndef TRAJECTORY_LOOP_FUNCTIONS_H
+#define TRAJECTORY_LOOP_FUNCTIONS_H
+
+#include <argos3/core/simulator/loop_functions.h>
+#include <buzz/argos/buzz_loop_functions.h>
+#include <argos3/plugins/simulator/entities/cylinder_entity.h>
+
+using namespace argos;
+
+class CTrajectoryLoopFunctions : public CBuzzLoopFunctions {
+
+public:
+
+   typedef std::map<CCylinderEntity*, std::vector<CVector3> > TWaypointMap;
+   TWaypointMap m_tWaypoints;
+   
+public:
+
+   CTrajectoryLoopFunctions() {}
+   virtual ~CTrajectoryLoopFunctions() {}
+
+   virtual void Init(TConfigurationNode& t_tree);
+
+   virtual void Reset();
+
+   virtual void PostStep();
+
+   inline const TWaypointMap& GetWaypoints() const {
+      return m_tWaypoints;
+   }
+
+   virtual void BuzzBytecodeUpdated();
+
+private:
+
+  /** The stimuli associated to the tasks */
+   std::vector<Real> m_vecStimuli;
+};
+
+#endif
