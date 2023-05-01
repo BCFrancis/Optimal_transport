4fde50017827b1b4781310ea75d5cea6a771f030
opletts <apratim101@gmail.com>
2023-04-30 20:10:40 -0400

    Add plotting

---
+++ graph.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+
+num_robots = 10
+name = 'box_' + str(num_robots) + '_box_data.dat'
+# Read data from the input file
+with open(name, 'r') as f:
+    lines = f.readlines()
+
+# Prepare data for plotting
+timesteps = []
+
+robot_list = []
+for i in range(num_robots):
+    robot_list.append('fb' + str(i))
+
+robot_positions = {robot_id: [] for robot_id in robot_list}
+average_positions = []
+
+for i, line in enumerate(lines):
+    timesteps.append(i)
+    tokens = line.split()
+    total_x, total_y = 0, 0
+
+    for j in range(0, len(tokens), 2):
+        robot_id = tokens[j]
+        x, y, _ = map(float, tokens[j+1].split(','))
+        robot_positions[robot_id].append((x, y))
+        total_x += x
+        total_y += y
+
+    # Calculate average position per time step
+    avg_x, avg_y = total_x / len(robot_positions), total_y / len(robot_positions)
+    average_positions.append((avg_x, avg_y))
+
+# Plot the positions
+fig, ax = plt.subplots()
+
+for robot_id, positions in robot_positions.items():
+    x_positions = [pos[0] for pos in positions]
+    y_positions = [pos[1] for pos in positions]
+    ax.plot(x_positions, y_positions, label=robot_id)
+    
+    # Plot the final position as a circle
+    final_x, final_y = x_positions[-1], y_positions[-1]
+    ax.scatter(final_x, final_y, s=100, marker='o', edgecolors='k')
+
+# Plot average positions
+avg_x_positions = [pos[0] for pos in average_positions]
+avg_y_positions = [pos[1] for pos in average_positions]
+ax.plot(avg_x_positions, avg_y_positions, label='Average', color='pink', linestyle='--')
+ax.scatter(avg_x_positions[-1], avg_y_positions[-1], s=150, marker='+', color='pink', edgecolors='k')
+ax.scatter(1.5, 0, s=150, marker='+', color='black' , edgecolors='k')
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_xlim([-2.5, 4.5])
+ax.set_ylim([-3, 3])
+ax.set_aspect('equal')
+ax.legend()
+ax.set_title(name)
+plt.show()
